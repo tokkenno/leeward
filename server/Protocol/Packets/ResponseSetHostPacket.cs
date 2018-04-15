4ad79b71972f4fd190c4d1ddef78aa221375d1be
Aitor González Fernández <reimashi@gmail.com>
2018-04-15 20:42:57 +0200

    ResponseSetHost packet implemented

---
+++ server/Protocol/Packets/ResponseSetHostPacket.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Leeward.Core;
+
+namespace Leeward.Protocol.Packets
+{
+    /// <summary>
+    /// Send zone host configuration to player
+    /// </summary>
+    internal class ResponseSetHostPacket : ResponsePacket
+    {
+        public readonly int PlayerId;
+        
+        public ResponseSetHostPacket(Player player) : base(PacketType.ResponseSetHost)
+        {
+            this.PlayerId = player.Id;
+        }
+
+        public override string ToString()
+        {
+            return $"Packet(ResponseSetHost) => PlayerId: {this.PlayerId}"; 
+        }
+
+        protected override void Write(BinaryWriter outWriter)
+        {
+            outWriter.Write(this.PlayerId);
+        }
+    }
+}
