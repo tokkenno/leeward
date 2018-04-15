0af34afbe9aa8f71a5825750bed3e1928e8e5c9d
Aitor González Fernández <reimashi@gmail.com>
2018-04-15 20:46:25 +0200

    ResponseLockZone packet implemented

---
+++ server/Protocol/Packets/ResponseLockZonePacket.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace Leeward.Protocol.Packets
+{
+    /// <summary>
+    /// Send zone lock status to player
+    /// </summary>
+    internal class ResponseLockZonePacket : ResponsePacket
+    {
+        public readonly bool LockStatus;
+        
+        public ResponseLockZonePacket(bool lockStatus = true) : base(PacketType.ResponseLockZone)
+        {
+            this.LockStatus = lockStatus;
+        }
+
+        public override string ToString()
+        {
+            return $"Packet(ResponseLockZone) => LockStatus: {this.LockStatus}"; 
+        }
+
+        protected override void Write(BinaryWriter outWriter)
+        {
+            outWriter.Write(this.LockStatus);
+        }
+    }
+}
