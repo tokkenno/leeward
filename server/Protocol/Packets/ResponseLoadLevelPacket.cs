4d1889344b27120a1db43f87db05cba940f3a349
Aitor González Fernández <reimashi@gmail.com>
2018-04-15 20:50:20 +0200

    ResponseLoadLevel packet implemented

---
+++ server/Protocol/Packets/ResponseLoadLevelPacket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Leeward.Protocol.Packets
+{
+    /// <summary>
+    /// Send zone name to player
+    /// </summary>
+    internal class ResponseLoadLevelPacket : ResponsePacket
+    {
+        public readonly String Name;
+        
+        public ResponseLoadLevelPacket(String name) : base(PacketType.ResponseLoadLevel)
+        {
+            this.Name = string.IsNullOrEmpty(name) ? "" : name;
+        }
+
+        public override string ToString()
+        {
+            return $"Packet(ResponseLoadLevel) => Name: {this.Name}"; 
+        }
+
+        protected override void Write(BinaryWriter outWriter)
+        {
+            outWriter.Write(this.Name);
+        }
+    }
+}
