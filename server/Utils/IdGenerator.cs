0e840b36290d8dca0915a2589f0f4991a6868a56
Aitor González Fernández <reimashi@gmail.com>
2018-04-15 05:50:58 +0200

    Id generators to separated files

--- server/Utils/IdGenerator.cs
+++ server/Utils/IdGenerator.cs
@@ -1,47 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Leeward.Utils
 {
-    internal interface IDGenerator
+    internal interface IdGenerator
     {
-        int Next();
-        void Free(int id);
-    }
-
-    internal class SequentialIdGenerator : IDGenerator
-    {
-        private readonly List<int> _ids = new List<int>();
-        private int _current = 0;
-        
-        public int Next()
-        {
-            lock (this._ids)
-            {
-                if (_ids.Count == _current)
-                {
-                    this._current += 1;
-                    this._ids.Add(this._current);
-                    return this._current;
-                }
-                else
-                {
-                    this._ids.Sort();
-                    int setNum = this._ids.TakeWhile(((elem, ind) => elem == ind)).Count();
-                    this._ids.Add(setNum);
-                    return setNum;
-                }
-            }
-        }
-
-        public void Free(int id)
-        {
-            lock (this._ids)
-            {
-                if (this._ids.Contains(id))
-                    this._ids.Remove(id);
-            }
-        }
+        UInt32 Next();
+        Int32 NextInt();
+        void Free(UInt32 id);
+        void FreeInt(Int32 id);
     }
 }
---
+++ server/Utils/SequentialIdGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Leeward.Utils
+{
+    internal class SequentialIdGenerator : IdGenerator
+    {
+        private readonly List<uint> _ids = new List<uint>();
+        private uint _current = 0;
+        
+        public uint Next()
+        {
+            lock (this._ids)
+            {
+                if (_ids.Count == _current)
+                {
+                    this._current += 1;
+                    this._ids.Add(this._current);
+                    return this._current;
+                }
+                else
+                {
+                    this._ids.Sort();
+                    uint setNum = (UInt32) this._ids.TakeWhile(((elem, ind) => elem == ind)).Count();
+                    this._ids.Add(setNum);
+                    return setNum;
+                }
+            }
+        }
+
+        public int NextInt()
+        {
+            return (int) this.Next(); 
+        }
+
+        public void Free(uint id)
+        {
+            lock (this._ids)
+            {
+                if (this._ids.Contains(id))
+                    this._ids.Remove(id);
+            }
+        }
+        
+        public void FreeInt(int id)
+        {
+            this.Free((uint) id);
+        }
+    }
+}
