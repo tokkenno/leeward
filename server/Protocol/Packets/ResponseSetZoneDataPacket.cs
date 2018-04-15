6c5f5b5bb035580508e1c5622677558f0770f018
Aitor González Fernández <reimashi@gmail.com>
2018-04-15 20:40:30 +0200

    ResponseSetZoneData packet implemented

---
+++ server/Protocol/Packets/ResponseSetZoneDataPacket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Leeward.IO;
+
+namespace Leeward.Protocol.Packets
+{
+    internal class ResponseSetZoneDataPacket : ResponsePacket
+    {
+        public readonly string ZoneData;
+        
+        public ResponseSetZoneDataPacket(String data) : base(PacketType.ResponseSetZoneData)
+        {
+            this.ZoneData = data;
+        }
+
+        public override string ToString()
+        {
+            return $"Packet(ResponseSetZoneData) => ZoneData?: {this.ZoneData}"; 
+        }
+
+        protected override void Write(BinaryWriter outWriter)
+        {
+            outWriter.Write(this.ZoneData);
+        }
+    }
+}
